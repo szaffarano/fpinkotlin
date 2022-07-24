24d27443a0bbee96aa57953775b7d43e9b45db35
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2022-07-24 08:25:28 +0200

    chore: Bump gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
-    kotlin("jvm") version "1.3.21"
-    kotlin("kapt") version "1.3.21"
+    id("org.jlleitschuh.gradle.ktlint")
+
+    kotlin("jvm")
+    kotlin("kapt")
 }
 
 val test by tasks.getting(Test::class) {
@@ -12,23 +13,26 @@ val test by tasks.getting(Test::class) {
 
 val arrowVersion = "0.10.2"
 dependencies {
-    compile(kotlin("stdlib"))
-    compile("io.arrow-kt:arrow-core-data:$arrowVersion")
-    compile("io.arrow-kt:arrow-fx:$arrowVersion")
-    compile("io.arrow-kt:arrow-mtl:$arrowVersion")
-    compile("io.arrow-kt:arrow-syntax:$arrowVersion")
-    compile("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.2")
-    compile("io.github.microutils:kotlin-logging:1.7.8")
-    compile("org.awaitility:awaitility:4.0.2")
-    runtime("org.slf4j:slf4j-simple:1.7.28")
+    implementation(kotlin("stdlib"))
+    implementation("io.arrow-kt:arrow-core-data:$arrowVersion")
+    implementation("io.arrow-kt:arrow-fx:$arrowVersion")
+    implementation("io.arrow-kt:arrow-mtl:$arrowVersion")
+    implementation("io.arrow-kt:arrow-syntax:$arrowVersion")
+    implementation(
+        "org.jetbrains.kotlinx:kotlinx-collections-immutable:0.2"
+    )
+    implementation("io.github.microutils:kotlin-logging:1.7.8")
+    implementation("org.awaitility:awaitility:4.2.0")
+    runtimeOnly("org.slf4j:slf4j-simple:1.7.36")
 
     // need this at compile level for chapter 8
-    compile("io.kotlintest:kotlintest-runner-junit5:3.3.2")
+    implementation("io.kotlintest:kotlintest-runner-junit5:3.3.2")
     kapt("io.arrow-kt:arrow-meta:$arrowVersion")
 }
 
 repositories {
     jcenter()
+    mavenCentral()
     maven("https://dl.bintray.com/kotlin/kotlinx")
 }
 
@@ -37,6 +41,7 @@ tasks.withType<KotlinCompile>().configureEach {
 }
 
 ktlint {
+    debug.set(false)
     verbose.set(true)
     disabledRules.set(
         setOf(
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,7 @@
-
+pluginManagement {
+    plugins {
+        id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
+        kotlin("jvm") version "1.3.21"
+        kotlin("kapt") version "1.3.21"
+    }
+}
